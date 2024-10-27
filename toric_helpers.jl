1edef9ef83d720cf3a23e44e3aabb94950db9e03
Mathilde Kermorgant <144392996+maathilde-k@users.noreply.github.com>
2024-10-27 19:18:49 -0400

    Update toric_helpers.jl

--- toric_helpers.jl
+++ toric_helpers.jl
@@ -1410,3 +1410,198 @@ function minMaxDivisorial(F::StackyFan,div::Dict{Vector{QQFieldElem}, Int64})
     end
     return divMaxConesRefined
 end
+
+######################################### HELPERS FOR D #########################################
+
+"""
+    divAlongDivisor(::Array{Int64,1},::StackyFan,::Dict)
+
+    Calculates the simplified divisorial index along a divisor of a given cone in a fan with divisorial rays. Takes the subcone consisting of all relevant residual rays along with the specified divisor, and counts the number of residual rays that are relevant with respect to that subcone.
+
+# Examples
+```jldoctest AlgDp
+julia> F=makeStackyFan([1 0 0; 0 1 0; 1 0 2],[[0,1,2]],[1,2,1])
+
+julia> div=Dict([1,0,0]=>0,[0,1,0]=>0,[1,0,2]=>1)
+    
+julia> divAlongDivisor([1,2,3],F,div,[1,0,2])
+2
+julia> div=Dict([1,0,0]=>0,[0,1,0]=>1,[1,0,2]=>1) #[1 0 0] [1 0 2] - [1 0 0] has non-zero projection
+    
+julia> divAlongDivisor([1,2,3],F,div,[1,0,2])
+1
+julia> div=Dict([1,0,0]=>1,[0,1,0]=>0,[1,0,2]=>1) #[0 1 0] has stacky value >1
+    
+julia> divAlongDivisor([1,2,3],F,div,[1,0,2])
+1
+julia> div=Dict([1,0,0]=>0,[0,1,0]=>1,[1,0,2]=>1) #[1 0 0] [0 1 0] - [1 0 0] is independent
+    
+julia> divAlongDivisor([1,2,3],F,div,[0,1,0])
+0
+```
+"""
+function divAlongDivisor(cone::Array{Int64,1}, F::StackyFan, div::Dict, divisor::Array{QQFieldElem,1})
+    # This may not be a very clear name, doesn't mention "simplified" or "index"
+    X=deepcopy(F)
+
+    slicedRayMatrix = getRays(X.fan)
+    rayMatrix = listToMatrix(slicedRayMatrix)
+    
+    # Construct the cone made up of the residual (non-divisorial) rays and the divisor
+    residRays = Array{QQFieldElem, 1}[]
+    residStack = QQFieldElem[]
+    c = 0
+    # Add the divisor (which should be divisorial)
+    if div[divisor] == 1
+        c += 1
+        push!(residRays, divisor)
+        push!(residStack, F.stacks[divisor])
+    else
+        print(divisor)
+        print("!!")
+        print(div[divisor])
+        error("The given ray is non-divisorial")
+    end     
+    # Append the other rays
+    for i in cone
+        ray = slicedRayMatrix[i]
+        stack = F.stacks[ray]
+        if div[ray] == 0 && isRelevant(ray, cone, F)
+            c += 1
+            push!(residRays, ray)
+            push!(residStack, stack)
+        end
+    end
+    residConeZ = Array(0:c-1)
+    residCone = Array(1:c)
+    # Create the stacky fan structure containing only the cone
+    residRayMatrix = Array{QQFieldElem}(transpose(hcat(residRays...)))
+    presubconeFan = normal_toric_variety(IncidenceMatrix([[i+1 for i in residConeZ]]), residRayMatrix)
+    subconeFan = addStackStructure(presubconeFan, residStack)
+
+    # Count the number of residual rays which are dependent in the above cone
+    # Skip the first ray, which is the divisor
+    divIndex = 0
+    for i in 2:c
+        ray = residRays[i]
+        if isRelevant(ray, residCone, subconeFan)
+            divIndex += 1
+        end
+    end
+    return divIndex
+end
+
+
+"""
+    positiveDivIndexCone()
+
+    Tests whether there exists a cone with non-zero divisorial index along the given divisor.
+"""
+function positiveDivIndexCone(divisor::Array{Int64,1}, F::StackyFan, div::Dict)
+    # Go through each maximal cone containing the ray, and testing the divisorial index
+    rayMatrix = convert(Array{Int64,2}, Array(Polymake.common.primitive(X.fan.RAYS)))
+    maxconeList = convertIncidenceMatrix(F.fan.MAXIMAL_CONES)
+    divisorIndex = getIndex(divisor, rayMatrix)
+    for maxCone in maxconeList
+        if divisorIndex in maxCone
+            coneDivIndex = divAlongDivisor(maxCone, F, div, divisor)
+            # If the divisorial index along the divisor is non-zero, return true
+            if coneDivIndex > 0
+                return true
+            end
+        end
+    end
+    return false
+end
+
+
+"""
+    minMaxDivisorial(::StackyFan,::Dict)
+    
+    Calculates the maximal divisorial index of all cones in a stacky fan. 
+    Each maximal cone of the fan will contain at most one minimal subcone of maximal divisorial index; a list of such cones is returned.
+
+# Examples
+```jldoctest
+julia> F=makeStackyFan([1 2 0;1 3 0; 3 0 1],[[0,1,2]],[1,1,5]);
+    
+julia> div=Dict([1,2,0]=>0,[1,3,0]=>0,[3,0,1]=>0);
+    
+julia> minMaxDivisorial(F,div)
+[[3]]
+    
+julia> F=makeStackyFan([1 1 0;1 3 0; 3 0 1],[[0,1,2]],[1,1,5]);
+
+julia> div=Dict([1,1,0]=>0,[1,3,0]=>0,[3,0,1]=>0);
+    
+julia> minMaxDivisorial(F,div)
+[[1,2,3]]
+```
+""" 
+function minMaxIndexAlongDiv(F::StackyFan,div::Dict{Vector{QQFieldElem}, Int64}, ray::Vector{QQFieldElem})
+    # Calculates the maximal divisorial index of any cone in the fan
+    divMax=0
+    coneList=getCones(F.fan)
+    # dictionary that represents each cone with its divisorial index
+    divisorialDict=Dict()
+    for cone in coneList
+        d=divAlongDivisor(cone,F,div, ray)
+        divisorialDict[cone]=d
+        if d>divMax
+            divMax=d
+        end
+    end 
+    if divMax==0
+        return nothing
+    end
+
+    # cones with maximal divisorial index
+    divMaxCones=Array{Int64,1}[]
+    for cone in coneList
+        if divisorialDict[cone]==divMax
+             # if the cone's divisorial index is the fan's maximal divisorial index, add the cone to divMaxCones
+            push!(divMaxCones,cone)
+        end
+    end
+
+    #divMaxConesRefined stores the cones in divMaxCones that are minimal with respect to inclusion
+    divMaxConesRefined=Array{Int64,1}[]
+    # List of maximal cones in F
+    maxconeList=getMaximalCones(F.fan)
+    for maxcone in maxconeList
+        # if the div index of the current maxcone is the fan's max div index, its minimal subcone with maximal divisorial index is calculated
+        if divisorialDict[maxcone]==divMax
+            maxconeContains=Array{Int64,1}[]
+            mincone=maxcone
+            for cone in divMaxCones
+                if coneContains(cone,maxcone) && size(cone,1)<size(mincone,1)
+                    mincone=cone
+                end
+            end
+            if !(mincone in divMaxConesRefined)
+                push!(divMaxConesRefined,mincone)
+            end
+        end
+    end
+    return divMaxConesRefined
+end
+
+function positiveDivIndexCone(divisor::Array{QQFieldElem,1}, F::StackyFan, div::Dict)
+    X = deepcopy(F)
+    while(true)
+        rayMatrix = listToMatrix(getRays(X.fan))
+        maxconeList = getMaximalCones(X.fan)
+        divisorIndex = getIndex(divisor, rayMatrix)
+        for maxCone in maxconeList
+            if divisorIndex in maxCone
+                coneDivIndex = divAlongDivisor(maxCone, F, div, divisor)
+                # If the divisorial index along the divisor is non-zero, return true
+                if coneDivIndex > 0
+                    return true
+                end
+            end
+        end
+    end
+    return false
+end
+
