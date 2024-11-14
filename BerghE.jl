318a1b08c4c995c4d809e05eccd1977f0303c98d
Mathilde Kermorgant <144392996+maathilde-k@users.noreply.github.com>
2024-11-14 17:45:56 -0500

    comments BerghE.jl

--- BerghE.jl
+++ BerghE.jl
@@ -2,6 +2,16 @@ include("toric_helpers.jl")
 include("BerghA.jl")
 include("BerghD.jl")
 
+"""
+    BerghE(F::StackyFan,divlist::Array{Int64,1})
+
+Given a stacky fan F and a vector of booleans divlist representing the distinguished structure, returns the result of a sequence of smooth stacky blowups such that the independency index is 0 everywhere at each step.
+
+The algorithm is adapted from Daniel Bergh's [paper on destackification](https://arxiv.org/abs/1409.5713). 
+# Examples
+
+"""
+
 function BerghE(F::StackyFan, divlist::Array{Int64,1})
     
     X = deepcopy(F)
@@ -13,37 +23,42 @@ function BerghE(F::StackyFan, divlist::Array{Int64,1})
     for i in 1:size(slicedRayMatrix,1)
         distVect[slicedRayMatrix[i]]=divlist[i]
     end
+    
     j= 0
     while(true)
-        #find the collection of minimal cones with maximal aggregrate 
+        ################# E1 : find the collection of minimal cones with maximal aggregrate ##################
         conelist = minMaxAggregate(X, distVect)
-        # if they equal the set of the maximal cones, we're done
         
+        # if they equal the set of the maximal cones, we're done
         if conelist == nothing
             break
         end
-        # otherwise, continue
-        # for each cone in the collection
-
+        
         blowupVectors = Vector{Vector{Int64}}([])
         rayConelist = []
+        
+        # for each cone in the collection of minimal cones with maximal aggregrate, add its rays to the rayConelist
         for cone in conelist
             raysOfCone = [getRays(F.fan)[i] for i in cone]
             push!(rayConelist,raysOfCone)
         end
 
+        
+        #for each set of rays representing a cone in rayConelist
         for rayCone in rayConelist
+
+            ################################# E2 : Blowup the worst locus ##################################
+            # create a cone containing all the rays of the rayCone
             allRays = getRays(X.fan)
             cone = findall(x -> x in rayCone, allRays)
             
-            ############################### working on the model
+            # create a model of the stacky fan corresponfing to the cone containing rayCone 
             modelRayMatrix = listToMatrix([getRays(X.fan)[i] for i in cone])#listToMatrix(getRays(X.fan))
             modelCone = Array{Int64}([i for i in [1:1:size(modelRayMatrix,1);]])
             modelConeCopy = [i for i in modelCone]
             modelStacks = [stackyWeights(X)[i] for i in cone]
             modelX = normal_toric_variety(IncidenceMatrix([modelConeCopy]),modelRayMatrix)
             modelSX = addStackStructure(modelX,modelStacks)
-        
             modelBlowupVector = findStackyBarycenter(cone, X)
             modelBlowupVector = map(x -> Int64(numerator(x)), modelBlowupVector)
             push!(blowupVectors,modelBlowupVector)
@@ -51,7 +66,6 @@ function BerghE(F::StackyFan, divlist::Array{Int64,1})
             modelGCD = gcd(modelBlowupVector)
             modelStacks = push!(modelStacks, modelGCD)
             NumOfRays = size(modelCone,1)
-            
             if size(modelCone,1) == 1
                 modelNewCone = [i for i in modelCone]
                 modelNewRays = modelRayMatrix
@@ -67,15 +81,17 @@ function BerghE(F::StackyFan, divlist::Array{Int64,1})
                     modelDist[i] = 1
                 end
             end
-
-
-            print(modelDist)
+            
+            ############################## E3: perform toric destackification ##############################
+            
+            # Perform Algorithm A on the model and record the steps taken for the blowup
             instructions = BerghAmod(modelNewSX, modelDist)[3]
 
+            ######################### E4: perform corresponding stacky blowups  ############################
             
-            ####################################
-        
             # working on the actual stacky fan
+            # perform the same sequence of blowups on the real stacky fan using instructions
+
             blowupVector = findStackyBarycenter(cone, X)
             blowupVectorPrim = Array{QQFieldElem}(Polymake.common.primitive(blowupVector))
             blowupVectorPrim = map(x -> Int64(numerator(x)), blowupVectorPrim)
@@ -83,9 +99,6 @@ function BerghE(F::StackyFan, divlist::Array{Int64,1})
             distVect[blowupVectorPrim] = 1
             push!(blowupVectors, blowupVectorPrim)
 
-        
-            #######################################
-        
             listOfRays = getRays(X.fan)
             newDivList = [0 for i in [1:1:size(listOfRays,1);]]
             for i in [1:1:size(listOfRays,1);]
@@ -93,33 +106,43 @@ function BerghE(F::StackyFan, divlist::Array{Int64,1})
                    newDivList[i] = 1 
                 end
             end
+            
             X, distVectList = manualBerghA(X, newDivList,instructions)
             newDistVect = Dict{Vector{QQFieldElem}, Int64}()
+            
             AllRays = getRays(X.fan)
             for i in [1:1:size(AllRays, 1);]
                 newDistVect[AllRays[i]] = distVectList[i]
             end
             distVect = newDistVect
         end
+        
+        ################### E5: eliminate divisorial index along distinguished divisors #####################
 
+        # To do so, we perform algorithm D repeatedly on the blowupVectors
+        
         blowupVectors = unique(blowupVectors)
 
         for blowupVector in blowupVectors
+        
             AllRays = getRays(X.fan)
+
+            # make a list of of distinguished divisors
             distVectList = Vector{Int64}([0 for i in [1:1:size(AllRays,1);]])
             for i in [1:1:size(AllRays,1);]
-                distVectList[i]= distVect[AllRays[i]]
+                distVectList[i] = distVect[AllRays[i]]
             end
 
+            # make a list labelling the blowup vector as the vector whose divisorial index we want to make 0.
             Dlist = Vector{Int64}([0 for i in [1:1:size(AllRays,1);]])
             for i in [1:1:size(AllRays,1);]
-                if Dlist[i] == blowupVector
+                if AllRays[i] == blowupVector
                     Dlist[i] = 1
                 end
             end
             
             X, distVect = BerghD(X, distVectList,Dlist)
-
+            
             newDistVect = Dict{Vector{QQFieldElem}, Int64}()
             allrays = getRays(X.fan)
             for i in [1:1:size(allrays, 1);]
